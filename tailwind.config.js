0ce4078ff1ffce1b423b55cf679b5425a429b68e
Okute <okute@Okutes-MacBook-Air.local>
2024-04-26 18:39:45 +0300

    html and tailwindcss design

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,25 @@ module.exports = {
       fontFamily: {
         JosefinSans: ['"Josefin Sans"', "sans-serif"],
       },
+      colors: {
+        // Colors
+
+        // Primary
+
+        "desaturated-red": "hsl(0, 36%, 70%)",
+        "soft-red": "hsl(0, 93%, 68%)",
+
+        // Neutral
+
+        "dark-grayish-red": "hsl(0, 6%, 24%)",
+
+        // Gradients
+
+        "linear-135deg-start": "hsl(0, 0%, 100%)",
+        "linear-135deg-stop": "hsl(0, 100%, 98%)",
+        "linear-135-deg-start": "hsl(0, 80%, 86%)",
+        "linear-135-deg-stop": "hsl(0, 74%, 74%)",
+      },
     },
   },
   plugins: [],
