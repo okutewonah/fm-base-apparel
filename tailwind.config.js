1adffc5601d925c528ff08daa7bf985daa152e44
Okute <okute@Okutes-MacBook-Air.local>
2024-04-28 17:03:08 +0300

    image url

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-mobile": "url('/images/hero-mobile.jpg')",
-        "hero-desktop": "url('/images/hero-desktop.jpg')",
-        "pattern-desktop": "url('/images/bg-pattern-desktop.svg')",
+        "hero-mobile": "url('./images/hero-mobile.jpg')",
+        "hero-desktop": "url('./images/hero-desktop.jpg')",
+        "pattern-desktop": "url('./images/bg-pattern-desktop.svg')",
       },
       fontFamily: {
         JosefinSans: ['"Josefin Sans"', "sans-serif"],
